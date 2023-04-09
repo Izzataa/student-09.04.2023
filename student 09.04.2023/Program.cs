6f24266045edcfce780856dc54ab917aee4184eb
izzataa <izzataa@code.edu.az>
2023-04-09 04:09:49 +0400

    Proje dosyası ekle.

---
+++ student 09.04.2023/Program.cs
@@ -0,0 +1,40 @@
+﻿
+internal class Student
+{
+    public string Name;
+    public string Surname;
+    public string Group;
+    public double Point;
+    public bool IsGraduated;
+
+    public Student(string Name, string Surname, string Group, double Point, bool IsGraudet)
+    {
+        this.Name = Name;
+        this.Surname = Surname;
+        this.Group = Group;
+        this.Point = Point;
+        this.IsGraduated = IsGraudet;
+    }
+
+    public string GetFullName()
+    {
+        return $"{Name} {Surname}";
+    }
+    public string GetAllInfo()
+    {
+        string info = $"Name: {Name}, Surname: {Surname}, Group: {Group}, Point: {Point}";
+        if (IsGraduated)
+        {
+            info += ",Graduated ";
+        }
+        else
+        {
+            info += ", Not Graduated";
+        }
+        return info;
+    }
+    public bool CanTakeNextExam()
+    {
+        return Point > 80;
+    }
+}
---
+++ student 09.04.2023/Stundent.cs
@@ -0,0 +1,9 @@
+﻿Student Student1 = new Student("Izzat", "Abdullayev", "P234", 85, true);
+Student Student2 = new Student("Hasan", "Qasimli", "A534", 63,false);
+
+
+Console.WriteLine(Student1.GetAllInfo());
+Console.WriteLine(Student1.CanTakeNextExam() ? "Can take the next exam." : "Cannot take the next exam.");
+
+Console.WriteLine(Student2.GetAllInfo());
+Console.WriteLine(Student2.CanTakeNextExam() ? "Can take the next exam." : "Cannot take the next exam.");
